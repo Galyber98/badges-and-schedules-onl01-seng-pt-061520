3de760dfff0e2f178bf8eb5f905dc31003215046
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-27 16:42:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,8 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
-  
+  end
+  assign_rooms(rooms).each do |assignment|
+    puts assignment
   end
 end
